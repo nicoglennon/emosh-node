ce6a037462e6ee81414c1ccb5bc2cdc912fcd568
Nico Glennon <nicoglennon@icloud.com>
2020-01-26 20:43:42 -0600

    Finish crud ops

--- controllers/index.js
+++ controllers/index.js
@@ -11,6 +11,78 @@ const createPost = async (req, res) => {
   }
 };
 
+const getAllPosts = async (req, res) => {
+  try {
+    const posts = await models.Post.findAll({
+      include: [
+        {
+          model: models.User,
+          as: "author"
+        }
+      ]
+    });
+    return res.status(200).json({ posts });
+  } catch (error) {
+    return res.status(500).send(error.message);
+  }
+};
+
+const getPostById = async (req, res) => {
+  try {
+    const { postId } = req.params;
+    const post = await models.Post.findOne({
+      where: { id: postId },
+      include: [
+        {
+          model: models.User,
+          as: "author"
+        }
+      ]
+    });
+    if (post) {
+      return res.status(200).json({ post });
+    }
+    return res.status(404).send("Post with the specified ID does not exist");
+  } catch (error) {
+    return res.status(500).send(error.message);
+  }
+};
+
+const updatePost = async (req, res) => {
+  try {
+    const { postId } = req.params;
+    const [updated] = await models.Post.update(req.body, {
+      where: { id: postId }
+    });
+    if (updated) {
+      const updatedPost = await models.Post.findOne({ where: { id: postId } });
+      return res.status(200).json({ post: updatedPost });
+    }
+    throw new Error("Post not found");
+  } catch (error) {
+    return res.status(500).send(error.message);
+  }
+};
+
+const deletePost = async (req, res) => {
+  try {
+    const { postId } = req.params;
+    const deleted = await models.Post.destroy({
+      where: { id: postId }
+    });
+    if (deleted) {
+      return res.status(204).send("Post deleted");
+    }
+    throw new Error("Post not found");
+  } catch (error) {
+    return res.status(500).send(error.message);
+  }
+};
+
 module.exports = {
-  createPost
+  createPost,
+  getAllPosts,
+  getPostById,
+  updatePost,
+  deletePost
 };
--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,10 @@ const controllers = require("../controllers");
 const router = Router();
 
 router.get("/", (req, res) => res.send("Welcome"));
-
+router.get("/posts", controllers.getAllPosts);
 router.post("/posts", controllers.createPost);
+router.get("/posts/:postId", controllers.getPostById);
+router.put("/posts/:postId", controllers.updatePost);
+router.delete("/posts/:postId", controllers.deletePost);
 
 module.exports = router;
