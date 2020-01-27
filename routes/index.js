67d9fcad219ffc42a5baa562035514681945e028
Nico Glennon <nicoglennon@icloud.com>
2020-01-26 22:45:18 -0600

    User Controller

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,15 @@ const controllers = require("../controllers");
 const router = Router();
 
 router.get("/", (req, res) => res.send("Welcome"));
+
+// user routes
+router.get("/users", controllers.getAllUsers);
+router.post("/users", controllers.createUser);
+router.get("/users/:userId", controllers.getUserById);
+router.put("/users/:userId", controllers.updateUser);
+router.delete("/users/:userId", controllers.deleteUser);
+
+// post routes
 router.get("/posts", controllers.getAllPosts);
 router.post("/posts", controllers.createPost);
 router.get("/posts/:postId", controllers.getPostById);
